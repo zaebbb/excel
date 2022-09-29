d27e7c9b083e4a9904b14528bbfc3d826b6e2cc5
zaebbb <elik@example.com>
2022-09-29 19:55:38 +0500

    finish DIP

---
+++ src/shared/LocalStorageClient.js
@@ -0,0 +1,23 @@
+import {storage, storageName} from '@core/utils';
+
+export class LocalStorageClient{
+    constructor(name){
+        this.name = storageName(name)
+    }
+
+    save(state){
+        storage(this.name, state)
+        return Promise.resolve()
+    }
+
+    get(){
+        return Promise.resolve(storage(this.name))
+        // return new Promise(resolve => {
+        //     const state = storage(this.name)
+        //
+        //     setTimeout(() => {
+        //         resolve(state)
+        //     }, 3000)
+        // })
+    }
+}
